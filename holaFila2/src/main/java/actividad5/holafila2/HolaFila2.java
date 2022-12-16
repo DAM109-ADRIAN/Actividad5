c24b5f691d13dffa5e85deb2de2f9fc5cc52b63d
DAM109-ADRIAN <adrian.dediegofernandez@iesmiguelherrero.com>
2022-12-16 20:02:05 +0100

    ejemplo1 de hola mundo 2

---
+++ holaFila2/src/main/java/actividad5/holafila2/HolaFila2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package actividad5.holafila2;
+
+/**
+ *
+ * @author DAM109
+ */
+public class HolaFila2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
